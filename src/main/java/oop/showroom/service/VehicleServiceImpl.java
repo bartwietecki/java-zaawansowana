526498a83d0e7e4c1165ddca8352af065121236c
bartwietecki <bartwietecki@gmail.com>
2023-01-11 21:05:39 +0100

    submit changes1

--- src/main/java/oop/showroom/service/VehicleServiceImpl.java
+++ src/main/java/oop/showroom/service/VehicleServiceImpl.java
@@ -3,7 +3,8 @@ package oop.showroom.service;
 import oop.showroom.model.Vehicle;
 import oop.showroom.repository.VehicleRepository;
 
-public class VehicleServiceImpl implements VehicleService {
+public class VehicleServiceImpl implements VehicleService { // implementacja interfejsów, robi się to po to aby
+                                                            // zaimplementować jakiś "service"
 
 
     // tutaj było zadanie z licznikiem, aby wypisywać po kolei każdy pojazd. Przenalizuję jeszcze raz to zadanie (nie zrobiłem)
